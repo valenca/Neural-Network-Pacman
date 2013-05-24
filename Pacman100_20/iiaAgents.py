bd946ddec2538dd732c7f45180d1df15106f1abf
Gustavo Martins <gus.a.martins@gmail.com>
2013-05-24 15:38:54 +0100

    mais cenas

---
+++ Pacman100_20/iiaAgents.py
@@ -0,0 +1,520 @@
+"""
+iiaAgents.py
+
+Created by Rui Lopes on 2012-02-18.
+Copyright (c) 2012 University of Coimbra. All rights reserved.
+"""
+
+"""
+Alterado por
+Alexandre Jesus - 2010130268
+Gustavo Martins - 2010131414
+Joao Valenca - 2010130607
+"""
+
+from pacman import Directions, SCARED_TIME, Actions
+from game import Agent
+from random import choice
+import util
+from util import getActionRepresentation, getStateRepresentation
+
+class iiaPacmanAgent(Agent):
+
+	def __init__(self, index = 0):
+		self.scaredCounter = 0
+		self.index = index
+		self.keys = []
+		self.trainingName = self.getTrainingName()
+
+		global trainingN
+		trainingN = self.trainingName
+
+		print "Training name:", self.trainingName
+		file = open(self.trainingName, 'w')
+		file.close()
+
+	def getTrainingName(self):
+		import os.path
+		name = "training_"
+		count = 1
+		while os.path.isfile("training/reactive/mediumClassic/all/" + name + str(count) + ".iia"):
+			count += 1
+		return "training/reactive/mediumClassic/all/" + name + str(count) + ".iia"
+		
+	def saveTraining(self, currentStuff):
+		import cPickle
+		try:
+			file = open(self.trainingName, 'r')
+			oldStuff = cPickle.load(file)
+			file.close()
+		except EOFError:
+			oldStuff = []
+		file = open(self.trainingName, 'w')
+		#print "Now saving:", oldStuff + [currentStuff]
+		#print "Adding:", currentStuff[0]
+		cPickle.dump(oldStuff + [currentStuff], file) 
+		file.close()
+
+	def convertState(self, state, stateRepresentation):
+		from copy import copy, deepcopy
+
+		direction = state.getPacmanState().getDirection()
+		stateR = deepcopy(stateRepresentation)
+
+		pos = []
+		if direction == Directions.NORTH:
+			return stateRepresentation
+		elif direction == Directions.SOUTH:
+			pos = [2,3,0,1,6,7,4,5,10,11,8,9,14,15,12,13,18,19,16,17]
+		elif direction == Directions.EAST or direction == Directions.STOP:
+			pos = [4,5,6,7,2,3,0,1,12,13,14,15,10,11,8,9,17,18,19,16]
+		elif direction == Directions.WEST:
+			pos = [6,7,4,5,0,1,2,3,14,15,12,13,8,9,10,11,19,16,17,18]
+
+		for i in range(20):
+			stateRepresentation[i] = stateR[pos[i]]
+
+		return stateRepresentation
+
+	def convertAction(self, state, actionRepresentation):
+		from copy import copy, deepcopy
+
+		direction = state.getPacmanState().getDirection()
+		actionR = deepcopy(actionRepresentation)
+
+		pos = []
+
+		if direction == Directions.NORTH:
+			return actionRepresentation
+		elif direction == Directions.SOUTH:
+			pos = [1, 0, 3, 2]
+		elif direction == Directions.EAST or direction == Directions.STOP:
+			pos = [2, 3, 1, 0]
+		elif direction == Directions.WEST:
+			pos = [3, 2, 0, 1]
+
+		for i in range(4):
+			actionRepresentation[i] = actionR[pos[i]]
+
+		return actionRepresentation
+
+	def getAction( self, state):
+		#from graphicsUtils import wait_for_keys
+		legal = state.getLegalActions(self.index)
+		stateRepresentation = getStateRepresentation(state)
+		#print "State Representation:", stateRepresentation
+		move = None
+		while move == None:
+			#self.keys = wait_for_keys()
+			move = self.getMove(state, stateRepresentation)
+			if move == None:
+				print "Illegal move. Try again"
+		
+		actionRepresentation = getActionRepresentation(move)
+		#print "Action Representation:", actionRepresentation
+		self.saveTraining([self.convertState(state, stateRepresentation), self.convertAction(state, actionRepresentation)])
+		return move
+
+	def getMove(self, state, stateRepresentation):
+		#Prioridades:
+			#Paredes
+			#Fantasmas Normais (fugir)
+			#Fantasmas Assustados (comer)
+			#Capsulas
+			#Pastilhas
+
+		#--- Exclui Paredes ---#
+		directions = state.getLegalActions()
+		#
+
+		#--- Exclui Paragem ---#
+		directions.remove(Directions.STOP)
+		#
+
+		#--- Exclui Fantasmas Normais ---#
+		for i in range(4):
+			#Linhas
+			if Directions.NUMBER[i] in directions:
+				if stateRepresentation[8+i*2] == 1 or stateRepresentation[8+i*2+1] == 1:
+					try:	 directions.remove(Directions.NUMBER[i])
+					finally: pass
+			#Diagonais
+			if Directions.NUMBER2[i] in directions:
+				if stateRepresentation[8+8+i%4] == 1 or stateRepresentation[8+8+(i+1)%4] == 1:
+					try:	 directions.remove(Directions.NUMBER2[i])
+					finally: pass
+
+		if len(directions) == 0:	return choice(state.getLegalActions())
+		elif len(directions) == 1:	return directions[0]
+		#
+
+		#--- Persegue Fantasmas Assustados ---#
+		sgDirections = []
+
+		for i in range(4):
+			#Linhas 1 casa
+			if Directions.NUMBER[i] in directions and Directions.NUMBER[i] not in sgDirections:
+				if stateRepresentation[8+i*2] == -1:
+					sgDirections.append(Directions.NUMBER[i])
+
+		if len(sgDirections) > 0: return choice(sgDirections)
+		
+		for i in range(4):
+			#Linhas 2 casas
+			if Directions.NUMBER[i] in directions and Directions.NUMBER[i] not in sgDirections:
+				if stateRepresentation[8+i*2+1] == -1:
+					sgDirections.append(Directions.NUMBER[i])
+			#Diagonais
+			if Directions.NUMBER2[i] in directions and Directions.NUMBER[i] not in sgDirections:
+				if stateRepresentation[8+8+i%4] == -1 or stateRepresentation[8+8+(i+1)%4] == -1:
+					sgDirections.append(Directions.NUMBER2[i])
+
+		if len(sgDirections) > 0: return choice(sgDirections)
+		#
+
+		#--- Persegue Capsulas ---#
+		if self.scaredCounter:		self.scaredCounter -= 1
+		else:
+			cDirections = []
+
+			for i in range(4):
+				if Directions.NUMBER[i] in directions and Directions.NUMBER[i] not in cDirections:
+					if stateRepresentation[i*2] == 1 and stateRepresentation[i*2+1] == 1:
+						cDirections.append(Directions.NUMBER[i])
+
+			if len(cDirections) > 0: return choice(cDirections)
+		#
+
+		#--- Persegue Pastilhas ---#
+		gDirections = []
+
+		for i in range(4):
+			if Directions.NUMBER[i] in directions and Directions.NUMBER[i] not in gDirections:
+				if stateRepresentation[i*2] == 1 and stateRepresentation[i*2+1] == 0:
+					gDirections.append(Directions.NUMBER[i])
+
+		if len(gDirections) > 0: return choice(gDirections)
+		#
+
+		try:	 directions.remove(Directions.REVERSE[state.getPacmanState().getDirection()])
+		finally: return choice(directions)
+
+
+class iiaGhostAgent(Agent):	
+	"""Uses a strategy pattern to allow usage of different ghost behaviors in the game. 
+	The strategy must receive an agent and a GameState as the arguments.
+	To set the strategy through command line use:
+	>>>pacman.py -g iiaGhostAgent --ghostArgs fnStrategy='fun1[;fun*]'
+	You may add new arguments as long as you provide a proper constructor. """
+	def __init__(self, index, fnStrategy='defaultstrategy'):
+		self.index=index
+		self.xCapsule = self.yCapsule = -1
+		strategies = fnStrategy.split(';')
+
+		try:
+			self.strategy = util.lookup(strategies[index%len(strategies)], globals())
+		except:
+			print "Function "+strategies[index%len(strategies)]+" not defined!"
+			self.strategy = default
+ 
+	def getAction(self, state):
+		"""The agent receives a GameState (defined in pacman.py).
+		 Simple random ghost agent."""
+		return self.strategy( self, state )
+	
+def default(agent,state):
+	return choice(state.getLegalActions(agent.index))
+
+def gowest(agent,state):
+	if Directions.WEST in state.getLegalActions(agent.index):
+		return Directions.WEST
+	else:
+		return choice(state.getLegalActions(agent.index))
+
+def gonorth(agent,state):
+	if Directions.NORTH in state.getLegalActions(agent.index):
+		return Directions.NORTH
+	else:
+		return choice(state.getLegalActions(agent.index))
+
+def goeast(agent,state):
+	if Directions.EAST in state.getLegalActions(agent.index):
+		return Directions.EAST
+	else:
+		return choice(state.getLegalActions(agent.index))
+
+def gosouth(agent,state):
+	if Directions.SOUTH in state.getLegalActions(agent.index):
+		return Directions.SOUTH
+	else:
+		return choice(state.getLegalActions(agent.index))
+
+#Fantasma Chaser
+def chaser(agent,state,fearful=False,directions=False):
+
+	if not fearful:
+		directions = state.getLegalActions(agent.index)
+
+	#Se vir o pacman nas diagonais, segue-o
+	withPacman = hasPacmanOnCorners(agent, state, directions)
+	if len(withPacman) >= 1:
+		return choice(withPacman)
+
+	#Se vir o pacman numa direcao, segue-o
+	withPacman = hasPacmanGhost(agent, state, directions)
+	if len(withPacman) == 1:
+		return withPacman[0]
+
+	return choice(directions)
+
+#Fantasma Fearful/Ambusher
+def fearful(agent,state):
+
+	directions = state.getLegalActions(agent.index)
+
+	#Se vir um fantasma tenta n ir atras dele
+	withoutGhosts = hasGhosts(agent, state, directions)
+	if len(withoutGhosts) == 1:
+		return withoutGhosts[0]
+	if len(withoutGhosts) > 1:
+		directions = withoutGhosts
+
+	#Se tiver assustado tenta fugir do pacman
+	if state.getGhostState(agent.index).scaredTimer:
+		withoutPacman = directions[:]
+		withPacmanCorners = hasPacmanOnCorners(agent, state, directions)
+		withPacman = hasPacmanGhost(agent, state, directions)
+		for i in withPacmanCorners:
+			if i in withoutPacman:
+				withoutPacman.remove(i)
+		for i in withPacman:
+			if i in withoutPacman:
+				withoutPacman.remove(i)
+
+		if len(withoutPacman) >= 1:
+			return choice(withoutPacman)
+		return choice(directions)
+
+	#Se nao tiver assustado comporta-se como o Chaser
+	return chaser(agent, state, 1, directions)
+
+#Fantasma Guardian
+def guardian(agent,state):
+	directions = state.getLegalActions(agent.index)
+
+	#Caso nao tenha capsula marcada vai procurar uma
+	if agent.xCapsule == -1:
+		capsulePos = isCapsule(agent, state, directions)
+		if capsulePos:
+			agent.xCapsule = capsulePos[0]
+			agent.yCapsule = capsulePos[1]
+
+	#Caso tenha uma capsula marcada ou acabado de encontrar uma vai verificar se ainda la esta (se a vir)
+	if agent.xCapsule != -1:
+		#Se consegue ver a posicao marcada
+		if seePosition(agent, state):
+			#Se a capsula ja la nao estiver
+			if not (agent.xCapsule, agent.yCapsule) in state.getCapsules():
+				agent.xCapsule = agent.yCapsule = -1
+
+	#Se vir que a capsula ainda esta la, ou se nao vir a marcacao da capsula, tenta ir para esta
+	if agent.xCapsule != -1:
+		return chooseDirection(agent, state, directions)
+
+	#Se nao tiver capsula marcada e nao tiver encontrado nenhuma, comporta-se como o Chaser
+	return chaser(agent, state, 1, directions)
+
+def hasPacmanOnCorners(agent, state, directions):
+	withPacman = []
+	x = state.getGhostPosition(agent.index)[0]
+	y = state.getGhostPosition(agent.index)[1]
+
+	#Acima esquerda
+	if (not state.hasWall(int(x) - 1, int(y) + 1)) and hasPacman(state, int(x) - 1, int(y) + 1):
+		if Directions.NORTH in directions:
+			withPacman.append(Directions.NORTH)
+		if Directions.LEFT in directions:
+			withPacman.append(Directions.LEFT)
+
+	#Acima direita
+	elif (not state.hasWall(int(x) + 1, int(y) + 1)) and hasPacman(state, int(x) + 1, int(y) + 1):
+		if Directions.NORTH in directions:
+			withPacman.append(Directions.NORTH)
+		if Directions.RIGHT in directions:
+			withPacman.append(Directions.RIGHT)
+
+	#Abaixo esquerda
+	elif (not state.hasWall(int(x) - 1, int(y) - 1)) and hasPacman(state, int(x) - 1, int(y) - 1):
+		if Directions.SOUTH in directions:
+			withPacman.append(Directions.SOUTH)
+		if Directions.LEFT in directions:
+			withPacman.append(Directions.LEFT)
+
+	#Abaixo direita
+	elif (not state.hasWall(int(x) + 1, int(y) - 1)) and hasPacman(state, int(x) + 1, int(y) - 1):
+		if Directions.SOUTH in directions:
+			withPacman.append(Directions.SOUTH)
+		if Directions.RIGHT in directions:
+			withPacman.append(Directions.RIGHT)
+
+	return withPacman
+
+#Verifica se o Pacman esta na linha de vista
+def hasPacmanGhost(agent,state,directions):
+	withPacman = directions[:]
+	for i in range(len(directions)):
+		if not isPacmanInDirection(agent, state, Actions._directions[directions[i]]):
+			withPacman.remove(directions[i])
+	return withPacman
+
+#Verifica se o Pacman esta numa direcao
+def isPacmanInDirection(agent,state,inc):
+	x = state.getGhostPosition(agent.index)[0] + inc[0]/2.0
+	y = state.getGhostPosition(agent.index)[1] + inc[1]/2.0
+
+	#Enquanto nao chegar a parede
+	while not state.hasWall(int(x), int(y)):
+		if hasPacman(state, x, y):
+			return True
+		x += inc[0]/2.0
+		y += inc[1]/2.0
+
+	return False
+
+#Devolve os caminhos sem fantasmas
+def hasGhosts(agent, state, directions):
+	withoutGhosts = directions[:]
+	for i in range(len(directions)):
+		if isGhostInDirection(agent, state, Actions._directions[directions[i]]):
+			withoutGhosts.remove(directions[i])
+	return withoutGhosts
+
+#Verifica se existe um fantasma numa direcao
+def isGhostInDirection(agent, state, inc):
+	x = state.getGhostPosition(agent.index)[0] + inc[0]/2.0
+	y = state.getGhostPosition(agent.index)[1] + inc[1]/2.0
+
+	#Enquanto nao chegar a parede
+	while not state.hasWall(int(x), int(y)):
+		if hasGhost(state, x, y, 0) or hasGhost(state, x, y, 1):
+			return True
+		x += inc[0]/2.0
+		y += inc[1]/2.0
+
+	return False
+
+#Devolve a posicao da capsula mais proxima
+def isCapsule(agent, state, directions):
+	xNearCapsule = yNearCapsule = -1
+	nearCapsule = -1
+	for i in range(len(directions)):
+		capsule = nearCapsuleInDirectionGhost(agent, state, Actions._directions[directions[i]])
+		if capsule[0] > 0 and (capsule[0] < nearCapsule or nearCapsule == -1):
+			xNearCapsule = capsule[1]
+			yNearCapsule = capsule[2]
+			nearCapsule = capsule[0]
+	return (xNearCapsule, yNearCapsule)
+
+#Devolve a distancia a uma capsula numa determinada direcao, devolve 0 se nao houver
+def nearCapsuleInDirectionGhost(agent, state, inc):
+	x = state.getGhostPosition(agent.index)[0] + inc[0]/2.0
+	y = state.getGhostPosition(agent.index)[1] + inc[1]/2.0
+	capsule = 1
+
+	while not state.hasWall(int(x), int(y)):
+		if (x, y) in state.getCapsules():
+			return (capsule, x, y)
+		x += inc[0]/2.0
+		y += inc[1]/2.0
+		capsule += 1
+
+	return (0, -1, -1)
+
+#Verifica se consegue ver determinada posicao do mapa
+def seePosition(agent, state):
+	xSelf = state.getGhostPosition(agent.index)[0]
+	ySelf = state.getGhostPosition(agent.index)[1]
+	inc = [-1, -1]
+
+	if xSelf == agent.xCapsule:
+		inc[0] = 0
+		if ySelf == agent.yCapsule:
+			return True
+		elif ySelf < agent.yCapsule:
+			inc[1] = 1
+		else:
+			inc[1] = -1
+	elif ySelf == agent.yCapsule:
+		inc[1] = 0
+		if xSelf < agent.xCapsule:
+			inc[0] = 1
+		else:
+			inc[0] = -1
+
+	if inc[0] != -1:
+		x = state.getGhostPosition(agent.index)[0]
+		y = state.getGhostPosition(agent.index)[1]
+		while not state.hasWall(int(x), int(y)):
+			if x == agent.xCapsule and y == agent.yCapsule:
+				return True
+			x += inc[0]/2.0
+			y += inc[1]/2.0
+
+	return False
+
+#Escolhe a direcao a tomar tendo em conta que tem uma capsula marcada
+def chooseDirection(agent, state, directions):
+	xSelf = state.getGhostPosition(agent.index)[0]
+	ySelf = state.getGhostPosition(agent.index)[1]
+
+	if xSelf < agent.xCapsule:
+		if ySelf < agent.yCapsule:
+			if Directions.NORTH in directions:
+				return Directions.NORTH
+			if Directions.EAST in directions:
+				return Directions.EAST
+		elif ySelf == agent.yCapsule:
+			return goeast(agent, state)
+		else:
+			if Directions.SOUTH in directions:
+				return Directions.SOUTH
+			if Directions.EAST in directions:
+				return Directions.EAST
+	elif xSelf == agent.xCapsule:
+		if ySelf < agent.yCapsule:
+			return gonorth(agent, state)
+		elif ySelf > agent.yCapsule:
+			return gosouth(agent, state)
+	else:
+		if ySelf < agent.yCapsule:
+			if Directions.NORTH in directions:
+				return Directions.NORTH
+			if Directions.WEST in directions:
+				return Directions.WEST
+		elif ySelf == agent.yCapsule:
+			return gowest(agent, state)
+		else:
+			if Directions.SOUTH in directions:
+				return Directions.SOUTH
+			if Directions.WEST in directions:
+				return Directions.WEST
+
+	return choice(directions)
+
+#Verifica se existe um Fantasma (assustado ou nao) numa determinada posicao
+def hasGhost(state, x, y, scared):
+	ghosts = state.getGhostPositions()
+	for i in xrange(len(ghosts)):
+		if not scared:
+			if state.getGhostState(i+1).scaredTimer == 0 and x == ghosts[i][0] and y == ghosts[i][1]:
+				return i+1
+		else:
+			if state.getGhostState(i+1).scaredTimer > 0 and x == ghosts[i][0] and y == ghosts[i][1]:
+				return i+1
+	return False
+
+#Verifica se existe o Pacman numa determinada posicao
+def hasPacman(state, x, y):
+	if x == state.getPacmanPosition()[0] and y == state.getPacmanPosition()[1]:
+		return True
+	return False
---
+++ Pacman100_20/learningAgents.py
@@ -0,0 +1,369 @@
+"""
+Alterado por
+Alexandre Jesus - 2010130268
+Gustavo Martins - 2010131414
+Joao Valenca - 2010130607
+"""
+
+from pacman import Directions
+import layout
+from game import Agent
+from random import choice, random, shuffle
+from util import loadFile, loadFilesFromList, loadFilesUntil, getStateRepresentation
+import math
+
+class NeuralNetworkAgent(Agent):
+
+	def __init__(self):
+		self.euler = math.e
+		self.learningFactor = 0.1
+		self.neurons = []
+		self.weights = []
+		self.biasWeights = []
+		self.nNeurons = range(20,5,-5)
+		self.nNeurons.append(5)
+		self.percTrainingFiles = 0.8
+		self.numFiles = 0
+		self.numCases = 0
+		self.correctCases = 0
+		self.stopCondition = 0.0000001
+		self.lastErrorsSize = 3
+		self.lastErrors = [1]*self.lastErrorsSize
+		self.lastErrorsIndex = 0
+		self.totalError = 0
+		self.averageTotalError = 0
+		self.minTrainingError = 2.5
+		self.averageTrainingError = 0
+		self.minValidationError = 2.5
+		self.averageValidationError = 0
+
+	def printNeurons(self):
+		for i in range(len(self.neurons)):
+			for j in range(len(self.neurons[i])):
+				print round(self.neurons[i][j], 3),
+			print 
+
+	def sig(self, value):
+		return (1.0/(1.0 + self.euler**(-value)))
+
+	def initialization(self):
+
+		#--- Inicializacao de valores de neuronios~---#
+		self.neurons.append([])
+		for i in self.nNeurons[1:]:
+			self.neurons.append([0]*i)
+		#
+
+		#--- Inicializacao de pesos [ -0.5 : 0.5 ] ---#
+		for i in range(len(self.nNeurons)-1):
+			self.weights.append([[random()-0.5 for j in range(self.nNeurons[i+1])] for k in range(self.nNeurons[i])])
+		for i in range(len(self.nNeurons)-1):
+			self.biasWeights.append([random()-0.5 for j in range(self.nNeurons[i+1])])
+		#
+
+
+	def trainNeuralNetwork(self, case):
+
+		#--- Coloca o input na primeira camada de neuronios ---#
+		self.neurons[0] = case[0]
+		#
+
+		#--- Criacao de array de erros e valor esperado ---#
+		deltas = []
+		deltas.append(case[0])
+		for i in self.nNeurons[1:]:
+			deltas.append([0]*i)
+		expected = case[1]
+		#
+
+		#--- Calculo de valores de neuronios e output ---#
+		for i in range(len(self.nNeurons)-1):
+			for j in range(self.nNeurons[i+1]):
+				for k in range(self.nNeurons[i]):
+					self.neurons[i+1][j] += self.neurons[i][k] * self.weights[i][k][j]
+				self.neurons[i+1][j] += self.biasWeights[(i+1)-1][j]
+				self.neurons[i+1][j] = self.sig(self.neurons[i+1][j])
+		#
+
+		#--- Calculo do erro total ---#
+		self.totalError = 0
+		for i in range(5):
+			self.totalError += (expected[i] - self.neurons[len(self.nNeurons)-1][i])**2
+		self.totalError /= 2
+		self.lastErrors[self.lastErrorsIndex] = self.totalError
+		self.lastErrorsIndex = (self.lastErrorsIndex + 1) % self.lastErrorsSize
+		#
+
+		#--- Estatisticas ---#
+		self.averageTrainingError += self.totalError
+		if self.totalError < self.minTrainingError:
+			self.minTrainingError = self.totalError
+		#
+
+		#--- Condicao de paragem ---#
+		if 1.0*sum(self.lastErrors)/len(self.lastErrors) < self.stopCondition:
+			return
+		#
+
+		#--- Diferenca entra e output esperado e obtido ---#
+		for i in range(5):
+			deltas[len(self.nNeurons)-1][i] = expected[i] - self.neurons[len(self.nNeurons)-1][i]
+		#
+		#--- Retropropagacao ---#
+		for i in range(len(self.nNeurons)-2, 0, -1):
+			for j in range(self.nNeurons[i]):
+				for k in range(self.nNeurons[i+1]):
+					deltas[i][j] += deltas[i+1][k] * self.weights[i][j][k]
+		#
+
+		#--- Recalculo dos pesos ---#
+		for i in range(len(self.nNeurons)-1):
+			for j in range(self.nNeurons[i]):
+				for k in range(self.nNeurons[i+1]):
+					self.weights[i][j][k] += self.learningFactor * deltas[i+1][k] * self.neurons[i][j] * (self.neurons[i+1][k] * (1 - self.neurons[i+1][k]))
+			for k in range(self.nNeurons[i+1]):
+				self.biasWeights[(i+1)-1][k] += self.learningFactor * deltas[i+1][k] * (self.neurons[i+1][k] * (1 - self.neurons[i+1][k]))
+		#
+
+	def validateNeuralNetwork(self, case):
+
+		#--- Coloca o input na primeira camada de neuronios ---#
+		self.neurons[0] = case[0]
+		#
+
+		#--- Criacao de array de valor esperado ---#
+		expected = case[1]
+		#
+
+		#--- Calculo de valores de neuronios e output ---#
+		for i in range(len(self.nNeurons)-1):
+			for j in range(self.nNeurons[i+1]):
+				self.neurons[i+1][j] = 0
+				for k in range(self.nNeurons[i]):
+					self.neurons[i+1][j] += self.neurons[i][k] * self.weights[i][k][j]
+				self.neurons[i+1][j] += self.biasWeights[(i+1)-1][j]
+				self.neurons[i+1][j] = self.sig(self.neurons[i+1][j])
+		#
+
+		#--- Calculo do erro total ---#
+		self.totalError = 0
+		for i in range(5):
+			self.totalError += (expected[i] - self.neurons[len(self.nNeurons)-1][i])**2
+		self.totalError /= 2
+		#
+
+		#--- Estatisticas ---#
+		self.averageValidationError += self.totalError
+		if self.totalError < self.minValidationError:
+			self.minValidationError = self.totalError
+		if expected[self.neurons[len(self.nNeurons)-1].index(max(self.neurons[len(self.nNeurons)-1]))] == 1:
+			self.correctCases += 1
+		#
+
+
+	#--- Le a rede neuronal do ficheiro ---#
+	def readNetwork(self, network):
+		import cPickle, os
+		file = open(network, 'r')
+		content = cPickle.load(file)
+		self.neurons = content[0]
+		self.weights = content[1]
+		self.biasWeights = content[2]
+		file.close()
+	#
+
+	#--- Guarda rede neuronal em ficheiro ---#
+	def writeNetwork(self, network):
+		import cPickle, os
+		file = open(network, 'w')
+		cPickle.dump([self.neurons, self.weights, self.biasWeights], file) 
+		file.close()
+	#
+
+	def loadNeuronalNetwork(self):
+		import cPickle, os
+
+		network = "network/network.iia"
+
+		#--- Se a rede ja estiver criada, apenas le os dados ---#
+		if os.path.isfile(network):
+			self.readNetwork(network)
+		#
+		#--- Caso contrario, cria-a, treina-a e guarda-a em ficheiro ---#
+		else:
+			#--- Le os ficheiros de treino ---#
+			fileNames = []
+			c = "Classic"
+			for i in ["human", "reactive"]:
+				for j in ["medium"+c]:
+					for k in ["all", "chaser", "guardian", "fearful", "default"]:
+						directory = "training/" + i + "/" + j + "/" + k
+						if os.path.isdir(directory):
+							for l in range(len(os.listdir(directory))):
+								fileNames.append(directory + "/training_" + str(l+1) + ".iia")
+			shuffle(fileNames)
+			#
+
+			#--- Criacao e inicializacao da rede neuronal ---#
+			self.initialization()
+			#
+
+			#--- Treino da rede neuronal ---#
+			self.numFiles = 0
+			self.numCases = 0
+			fileNamesTraining = fileNames[:int(len(fileNames)*self.percTrainingFiles)]
+			
+			files = loadFilesFromList(fileNamesTraining)
+			shuffle(files)
+			for file in files:
+				self.numFiles += 1
+				self.learningFactor = 1.0*file[0]/10000
+				file = file[1:]
+				shuffle(file)
+				print self.numFiles
+				for case in file:
+					self.numCases += 1
+					self.trainNeuralNetwork(case)
+					#--- Condicao de paragem ---#
+					if 1.0*sum(self.lastErrors)/len(self.lastErrors) < self.stopCondition:
+						break
+					#
+				#--- Condicao de paragem ---#
+				if 1.0*sum(self.lastErrors)/len(self.lastErrors) < self.stopCondition:
+					break
+				#
+
+			print
+
+			print "Number of Files Trained:", self.numFiles
+			print "Number of Cases Trained:", self.numCases
+			print "Minimum Training Total Error:", self.minTrainingError
+			print "Average Training Total Error:", self.averageTrainingError/self.numCases
+
+			self.writeNetwork(network)
+			#
+
+			print
+
+			#--- Validacao da rede neuronal ---#
+			self.numFiles = 0
+			self.numCases = 0
+			self.correctCases = 0
+			fileNamesValidation = fileNames[int(len(fileNames)*self.percTrainingFiles):]
+
+			files = loadFilesFromList(fileNamesValidation)
+			shuffle(files)
+			for file in files:
+				self.numFiles += 1
+				file = file[1:]
+				shuffle(file)
+				for case in file:
+					self.numCases += 1
+					self.validateNeuralNetwork(case)
+
+			print "Number of Files Validated:", self.numFiles
+			print "Number of Cases Validated:", self.numCases
+			print "Minimum Validation Total Error:", self.minValidationError
+			print "Average Validation Total Error:", self.averageValidationError/self.numCases
+			print "Number of Correct Cases:", self.correctCases
+			print "Percentage of Correct Cases:", str(round(1.0*self.correctCases/self.numCases*100, 2)) + "%"
+
+			print
+			#
+	#
+
+	def convertState(self, state, stateRepresentation):
+		from copy import copy, deepcopy
+
+		direction = state.getPacmanState().getDirection()
+		stateR = deepcopy(stateRepresentation)
+
+		pos = []
+		if direction == Directions.NORTH:
+			return stateRepresentation
+		elif direction == Directions.SOUTH:
+			pos = [2,3,0,1,6,7,4,5,10,11,8,9,14,15,12,13,18,19,16,17]
+		elif direction == Directions.EAST or direction == Directions.STOP:
+			pos = [4,5,6,7,2,3,0,1,12,13,14,15,10,11,8,9,17,18,19,16]
+		elif direction == Directions.WEST:
+			pos = [6,7,4,5,0,1,2,3,14,15,12,13,8,9,10,11,19,16,17,18]
+
+		for i in range(20):
+			stateRepresentation[i] = stateR[pos[i]]
+
+		return stateRepresentation
+
+	def deconvertAction(self, state, actionRepresentation):
+		from copy import copy, deepcopy
+
+		direction = state.getPacmanState().getDirection()
+		actionR = deepcopy(actionRepresentation)
+
+		pos = []
+
+		if direction == Directions.NORTH:
+			return actionRepresentation
+		elif direction == Directions.SOUTH:
+			pos = [1, 0, 3, 2]
+		elif direction == Directions.EAST or direction == Directions.STOP:
+			pos = [3, 2, 0, 1]
+		elif direction == Directions.WEST:
+			pos = [2, 3, 1, 0]
+
+		for i in range(4):
+			actionRepresentation[i] = actionR[pos[i]]
+
+		return actionRepresentation
+		
+	def getMove(self, stateRepresentation):
+
+		#--- Coloca o input na primeira camada de neuronios ---#
+		self.neurons[0] = stateRepresentation
+		#
+
+		#--- Calculo de valores de neuronios e output ---#
+		for i in range(len(self.nNeurons)-1):
+			for j in range(self.nNeurons[i+1]):
+				self.neurons[i+1][j] = 0
+				for k in range(self.nNeurons[i]):
+					self.neurons[i+1][j] += self.neurons[i][k] * self.weights[i][k][j]
+				self.neurons[i+1][j] += self.biasWeights[(i+1)-1][j]
+				self.neurons[i+1][j] = self.sig(self.neurons[i+1][j])
+		#
+
+		return self.neurons[len(self.nNeurons)-1]
+
+	def getDirection(self, state, actions):
+		pacmanPos = state.getPacmanPosition();
+
+		while True:
+
+			randNumber = random() * sum(actions[:-1])
+
+			if(randNumber >= 0 and randNumber < actions[0]):
+				if not state.hasWall(pacmanPos[0], pacmanPos[1]+1):
+					return Directions.NORTH
+			elif(randNumber >= actions[0] and randNumber < sum(actions[0:2])):
+				if not state.hasWall(pacmanPos[0], pacmanPos[1]-1):
+					return Directions.SOUTH
+			elif(randNumber >= sum(actions[0:2]) and randNumber < sum(actions[0:3])):
+				if not state.hasWall(pacmanPos[0]+1, pacmanPos[1]):
+					return Directions.EAST
+			elif(randNumber >= sum(actions[0:3]) and randNumber < sum(actions[0:4])):
+				if not state.hasWall(pacmanPos[0]-1, pacmanPos[1]):
+					return Directions.WEST
+
+
+	def getAction(self, state):
+
+		self.loadNeuronalNetwork()
+
+		actions = self.getMove(self.convertState(state, getStateRepresentation(state)))
+		print self.convertState(state, getStateRepresentation(state))
+		actions = self.deconvertAction(state, actions)
+		print actions
+
+		return Directions.NUMBER[actions.index(max(actions))]
+		#return self.getDirection(state, actions)
+
+		
